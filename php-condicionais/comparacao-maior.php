f24c3f3cb965fd329b84f46d2d416cb211594674
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-04-08 11:53:48 -0300

    adicionando exercício 01 (comparação maior e menor) - aula 02

---
+++ php-condicionais/comparacao-maior.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Definir duas variáveis com números (à seu critério), comparar usando if para
+decidir qual deles é maior e imprimir a frase “O maior número é ___”, sendo
+que ___ recebe o valor da maior variável que você criou.
+a. Adicionar um else ao código anterior para cobrir o caso inverso. */
+
+$var1 = 15;
+$var2 = 9;
+
+if ($var1 > $var2) {
+    echo "O maior número é $var1";
+} else {
+    echo "O maior número é $var2";
+}
+
+?>

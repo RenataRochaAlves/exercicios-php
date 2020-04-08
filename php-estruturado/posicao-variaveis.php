b40a61e98cbbf35e6ac4b86425276e37e2e20937
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-04-08 00:44:36 -0300

    adicionando exercício 02 - aula 01

---
+++ php-estruturado/posicao-variaveis.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 2. Com base na frase “Três pratos de trigo para três tigres tristes”, criar uma variável
+para cada palavra, de forma que o nome de cada variável indique a posição da
+palavra na frase.
+a. É possível atribuir um número ($1, $2, $3) como nome da variável?
+b. Atribuir a cada variável sua posição numérica escrita como texto ($um,
+$dois, $tres).
+c. Imprimir a frase usando todas as variáveis declaradas.
+d. Imprimir uma frase (diferente da feita acima) usando pelo menos duas
+variáveis já declaradas no ponto anterior. */
+
+$um = "Três";
+$dois = "pratos";
+$tres = "de";
+$quatro = "trigo";
+$cinco = "para";
+$seis = "três";
+$sete = "tigres";
+$oito = "tristes";
+
+echo $um ." ". $dois ." ". $tres ." ". $quatro ." ". $cinco ." ". $seis ." ". $sete ." ". $oito ."<br>";
+
+echo $um ." ". $sete ." ". $cinco ." ". $seis ." ". $dois;
+
+?>

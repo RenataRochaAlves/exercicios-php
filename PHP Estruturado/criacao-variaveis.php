d432448c7b2520ade2de36ddfea2649a051ef316
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-04-08 00:26:48 -0300

    adicionando exercício 01 - aula 01

---
+++ PHP Estruturado/criacao-variaveis.php
@@ -0,0 +1,27 @@
+<?php 
+
+/* 1. Criar as seguintes variáveis e mostrá-las usando echo e var_dump:
+a. Uma variável com um número inteiro.
+b. Uma variável com um número decimal.
+c. Uma variável com um caractere, usando aspas duplas.
+d. Uma variável com um caractere, usando aspas simples.
+e. Uma variável com uma string, usando aspas simples.
+f. Uma variável com uma string,, usando aspas duplas.
+g. Atribuir uma string de caracteres à variável do ponto a.
+h. Atribuir um número decimal à variável do ponto e.
+i. O que acontece com o tipo de dado das variáveis do ponto g e do ponto h?
+j. O que acontece se atribuirmos outro valor numérico à variável do ponto a,
+mas entre aspas? */
+
+$inteiro = 10;
+$decimal = 0.5;
+$caractereDupla = "a";
+$caractereSimples = 'a';
+$stringDupla = "Isso é uma string";
+$stringSimples = 'Isso é outra string';
+
+$inteiro = "string de caracteres";
+
+$stringSimples = 0.75;
+
+?>

1b5cfbe2cf47b1c56dfe6abd1be0ce7766e18a0f
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-04-15 18:26:27 -0300

    adicionando exercício 08 (if ternario) - aula 02

---
+++ php-condicionais/if-ternario.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Definir uma variável $numero com um número. Imprimir “O número é par” ou
+// “O número é impar”, de acordo com a situação.
+// Este exercício deve ser resolvido com um IF Ternário.
+
+$numero = 10;
+
+echo ($numero % 2 ==0) ? "O número é par" : "O número é impar";
+
+?>
